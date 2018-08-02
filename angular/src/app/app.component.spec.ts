08064533ce23a2dc25e9ed8360b1757d1d5d66df
Benedikt Straube <benediktstraube@hotmail.de>
2018-08-02 08:57:43 +0200

    fixed unit tests

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        HttpClientModule,
+        FormsModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +20,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Verbindung zur Cassandra Datenbank über Container'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Verbindung zur Cassandra Datenbank über Container');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Verbindung zur Cassandra Datenbank über Container');
   }));
 });

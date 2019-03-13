b9354cd54b4c4fc51cc37b763e59227ff1d9d724
MarvinK97 <marvin.kohlmann@atos.net>
2019-03-13 08:46:38 +0100

    added travis config copy

--- scripts/springCassScript.sh
+++ scripts/springCassScript.sh
@@ -2,6 +2,7 @@
 #script file for tests regarding the spring application.
 
 cd ./springCass
+cp src/test/resources/configs/application.properties src/test/resources/application.properties
 gradle test
 #here you could add your test for Spring. keep in mind to track whether the command to run the test fails as you can see below,
 #since the script otherwise won´t fail and exit with 0, causing travis to think that all tests were succsesfull.

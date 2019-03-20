ab3a304928142552d8ccc1b731c0cdbf5be52346
Ferias <christian.beilborn@gmx.de>
2019-03-20 15:04:34 +0100

    Array eingebaut

--- angular/e2e/feedback/feedback.e2e-spec.ts
+++ angular/e2e/feedback/feedback.e2e-spec.ts
@@ -1,6 +1,14 @@
 import { FeedbackPage } from './feedback.po';
 import { browser, by, element } from 'protractor';
 
+// Variablen für CSV-Datei zu lesen
+const file_system = require('fs');
+const csvModule = require('papaparse');
+const strSomething = file_system.readFileSync('e2e\\csv-data.csv', 'utf8');
+const csvValues = csvModule.parse(strSomething, {
+  //  header:true
+  // delimeter:','
+});
 
 describe('Feedback', () => {
   let page: FeedbackPage;
@@ -9,6 +17,8 @@ describe('Feedback', () => {
     page = new FeedbackPage();
   });
 
+  for(let i=0; i<csvValues.data.length-1; i++) {
+    let j = 0;
   // Check if Feedback-Page is loaded
   it('should load Home-Page ', () => {
     page.navigateToFeedback();
@@ -16,11 +26,16 @@ describe('Feedback', () => {
   });
 
   // Check if textfield appears after star-rating, submit and check if it is approved
-  it('feedback page opens and elements are dynamically', () => {
-    page.navigateToFeedback();
-    page.setStarRating();
-    expect(element(page.getTextField).isDisplayed()).toBe(true);
-    page.submitFeedback();
-    expect(element(page.getThanks).isDisplayed()).toBe(true);
-  });
+    it('feedback page opens and elements are dynamically', () => {
+      page.navigateToFeedback();
+      expect(page.getParagraphText()).toEqual('Feedback');
+      page.setStarRating();
+      expect(element(page.getTextField).isDisplayed()).toBe(true);
+      element.all(by.css('.star')).get(csvValues.data[i][j] - 1).click(); //int
+      j += 1;
+      element(by.id('reFeedbackText')).sendKeys(csvValues.data[i][j]); //string ha
+      page.submitFeedback();
+      expect(element(page.getThanks).isDisplayed()).toBe(true);
+    });
+  };
 });

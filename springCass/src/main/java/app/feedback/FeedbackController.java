5b63f6da671132e7625b779d6b37909de499fb08
Lucas Golenia <lucas.golenia@atos.net>
2019-03-28 11:30:12 +0100

    optimized imports

--- springCass/src/main/java/app/feedback/FeedbackController.java
+++ springCass/src/main/java/app/feedback/FeedbackController.java
@@ -1,18 +1,12 @@
 package app.feedback;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 
 @RestController

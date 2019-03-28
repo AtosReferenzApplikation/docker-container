3f0df68ad501b551d1d57c99901cccb11666869c
Lucas Golenia <lucas.golenia@atos.net>
2019-03-28 14:30:23 +0100

    added delete route

--- springCass/src/main/java/app/customer/CustomerController.java
+++ springCass/src/main/java/app/customer/CustomerController.java
@@ -1,21 +1,21 @@
 package app.customer;
 
-//import java.util.*;
 import java.util.List;
-import java.util.Arrays;
-import com.google.common.collect.Lists;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+import com.google.common.collect.Lists;
 
 
 @RestController
@@ -34,9 +34,15 @@ public class CustomerController {
 
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/getCustomers")
-    public String getCustomers() {
-      List<Customer> customerList = Lists.newArrayList(customerRepository.findAll());
-      return customerList.toString();
+    public @ResponseBody List<Customer> getCustomers() {
+    	List<Customer> customerList = Lists.newArrayList(customerRepository.findAll());
+      	return customerList;
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/deleteCustomer/{id}", method = RequestMethod.DELETE)
+    public void deleteCustomer(@PathVariable("id") String itemId){
+    	customerRepository.deleteById(UUID.fromString(itemId));
     }
 
     //Kafka Producer

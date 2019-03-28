e0a07a6a61dc0e9a8652a2e09afc3e25790eec65
Lucas Golenia <lucas.golenia@atos.net>
2019-03-28 11:27:55 +0100

    optimized routing

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AtosComponent } from './atos/atos.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { CircuitComponent } from './circuit/circuit.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     component: AtosComponent
@@ -19,12 +25,16 @@ const routes: Routes = [
   },
   {
     path: '**',
-    component: AtosComponent
+    redirectTo: 'home'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }

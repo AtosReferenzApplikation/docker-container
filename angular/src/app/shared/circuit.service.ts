1ef825bcb852aef627fe7d703c6e3f463a2e624e
Lucas Golenia <lucas.golenia@atos.net>
2019-04-04 15:24:38 +0200

    added circuit sdk

--- angular/src/app/shared/circuit.service.ts
+++ angular/src/app/shared/circuit.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Customer } from '../models/customer';
+import Circuit from 'circuit-sdk';
+// import * as Circuit from 'circuit-sdk';
 
 @Injectable({
   providedIn: 'root'
@@ -16,22 +17,54 @@ export class CircuitService {
     .set('Accept', 'application/json')
     .set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
 
-  private _loggedIn = false;
+  // sdk
+  client; // Circuit SDK instance
+  user: any; // Logged on user
+  call: any; // Active call object
+  connectionState: string = Circuit.Enums.ConnectionState.Disconnected;
+  public addEventListener: Function;
+  // OAuth configuration. Get your own client_id for your app at https://circuit.github.io/oauth.html
+  oauthConfig = {
+    domain: 'circuitsandbox.net',
+    client_id: '8e3edf9798f341c08ae59b5d8cf74341',
+    redirect_uri: 'http://localhost:4200/circuit',
+    scope: 'ALL'
+  };
 
   constructor(private http: HttpClient) {
     this.refreshToken();
+
+    // Set Circuit SDK internal log level
+    Circuit.logger.setLevel(Circuit.Enums.LogLevel.Debug);
+
+    // Create the Circuit SDK client using Implicit grant type
+    // See http://circuit.github.com/oauth
+    this.client = new Circuit.Client({
+      client_id: this.oauthConfig.client_id,
+      domain: this.oauthConfig.domain,
+      scope: this.oauthConfig.scope
+    });
+
+    // Bind event listener directly to SDK's addEventListener
+    this.addEventListener = this.client.addEventListener.bind(this);
+
+    // Keep the call object current in this service
+    this.client.addEventListener('callIncoming', evt => this.call = evt.call);
+    this.client.addEventListener('callStatus', evt => this.call = evt.call);
+    this.client.addEventListener('callEnded', evt => this.call = null);
   }
 
-  get loggedIn(): boolean {
-    return this._loggedIn;
-  }
+  /**************
+   *
+   * AUTHENTICATION
+   *
+   *******************/
 
-  // Authentication
   authenticateUser() {
-    const client_id = '';
     const state = Math.random().toString(36).substr(2, 15);
-    const scopes = 'ALL';
-    const url = this.authUrl + '?response_type=token&client_id=' + client_id + '&redirect_uri=http://localhost:4200/circuit&scope=' + scopes + '&state=' + state;
+    const url = this.authUrl + '?response_type=token&client_id=' + this.oauthConfig.client_id
+      + '&redirect_uri=http://localhost:4200/circuit&scope=' + this.oauthConfig.scope
+      + '&state=' + state;
 
     const loginPopup = window.open(url, 'Circuit Authentication', 'centerscreen,location,resizable,alwaysRaised,width=400,height=504');
 
@@ -70,15 +103,58 @@ export class CircuitService {
           .set('Content-Type', 'application/json')
           .set('Accept', 'application/json')
           .set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
-        this._loggedIn = true;
+
+        this.logonWithSpinner(localStorage.getItem('access_token'));
       }).catch(() => {
-        this._loggedIn = false;
         // request user to logIn again
         // catch error 401
       });
   }
 
-  // API Calls
+
+  /**************
+   *
+   * CIRCUIT SDK
+   *
+   *******************/
+
+  // Logon to Circuit using the access token and show spinner while looging in.
+  private logonWithSpinner(token) {
+    // const loading = this.loadingCtrl.create({ content: 'Signing in...' });
+    // loading.present();
+    return this.client.logon({ accessToken: token, skipTokenValidation: true })
+      .then(user => {
+        // loading.dismiss();
+        console.log('client logon success', user)
+        return user;
+      })
+      .catch(err => {
+        window.localStorage.removeItem('access_token');
+        // loading.dismiss();
+        console.log('client logon error', err)
+        return Promise.reject(err);
+      });
+  }
+
+  // Logout of Circuit
+  logout() {
+    return this.client.logout();
+  }
+
+  // Starts video/audio call with the specified user, conversation will be created if it does not exist.
+  startCall(email: string, video: boolean): Promise<any> {
+    return this.client.makeCall(email, { audio: true, video: !!video }, true)
+      .then(call => this.call = call)
+      .catch(console.error);
+  }
+
+
+  /**************
+   *
+   * REST API
+   *
+   *******************/
+
   getAllConversations(results: string = '25') {
     const params = new HttpParams()
       .set('direction', 'BEFORE')
--- angular/src/typings.d.ts
+++ angular/src/typings.d.ts
@@ -3,3 +3,8 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+// declare module 'circuit-sdk' {
+//   var Circuit: any;
+//   export = Circuit;
+// }

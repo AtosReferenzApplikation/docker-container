aef32c47bb1cbef0c2ed3201b577dcab32392a8d
LucasG04 <lucas.golenia@atos.net>
2019-04-05 11:41:23 +0200

    added sample customers

---
+++ angular/src/app/shared/sample-customers.ts
@@ -0,0 +1,8 @@
+import { Customer } from '../models/customer';
+
+export const SAMPLE_CUSTOMERS: Customer[] = [
+    {id: '01', name: 'Peter', surname: 'Meier', email: 'peter.meier99@gmx.de', phone: '+4915233742229'},
+    {id: '02', name: 'Sabine', surname: 'Blau', email: 'peter.meier99@gmx.de', phone: '+4915233742229'},
+    {id: '03', name: 'Hubert', surname: 'Schmidt', email: 'peter.meier99@gmx.de', phone: '+4915233742229'},
+    {id: '04', name: 'Alexa', surname: 'Bezos', email: 'peter.meier99@gmx.de', phone: '+4915233742229'},
+]

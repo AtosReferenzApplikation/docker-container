a505da8a82207bdae472a4f52e905a163531072c
Lucas Golenia <lucas.golenia@atos.net>
2019-04-10 14:28:08 +0200

    kunden model erweitert #88

--- angular/src/app/models/customer.ts
+++ angular/src/app/models/customer.ts
@@ -1,12 +1,16 @@
 export interface Customer {
   id: string;
+  salutation: string;
   name: string;
   surname: string;
   email: string;
   phone: string;
+  nationality: string;
   address: {
+    country: string;
     postalcode: string;
     city: string;
     street: string;
   }
+  contactform: string[];
 }

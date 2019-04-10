5a0ef24fd8a04379897cf13526fafb821bbf9960
Lucas Golenia <lucas.golenia@atos.net>
2019-04-10 14:28:08 +0200

    kunden model erweitert #88

--- angular/src/app/shared/sample-customers.ts
+++ angular/src/app/shared/sample-customers.ts
@@ -3,23 +3,27 @@ import { Customer } from '../models/customer';
 export const SAMPLE_CUSTOMERS: Customer[] = [
 
   {
-    id: '01', name: 'Peter', surname: 'Meier', email: 'peter.meier99@gmx.de', phone: '+4915233742229', address: {
-      postalcode: '90762', city: 'Fürth', street: 'Lange Str. 7'
-    }
+    id: '01', salutation: 'Herr', name: 'Peter', surname: 'Meier',
+    email: 'peter.meier99@gmx.de', phone: '+4915233742229', nationality: 'deutsch', address: {
+      country: 'Deutschland', postalcode: '90762', city: 'Fürth', street: 'Lange Str. 7'
+    }, contactform: ['E-Mail', 'Telefon', 'Post']
   },
   {
-    id: '02', name: 'Sabine', surname: 'Blau', email: 'peter.meier99@gmx.de', phone: '+4915233742229', address: {
-      postalcode: '08523', city: 'Plauen', street: 'Bergstraße 36'
-    }
+    id: '02', salutation: 'Frau', name: 'Sabine', surname: 'Blau',
+    email: 'peter.meier99@gmx.de', phone: '+4915233742229', nationality: 'deutsch', address: {
+      country: 'Deutschland', postalcode: '08523', city: 'Plauen', street: 'Bergstraße 36'
+    }, contactform: ['E-Mail', 'Telefon', 'Post']
   },
   {
-    id: '03', name: 'Hubert', surname: 'Schmidt', email: 'peter.meier99@gmx.de', phone: '+4915233742229', address: {
-      postalcode: '33609', city: 'Bielefeld', street: 'Hakenort 45'
-    }
+    id: '03', salutation: 'Herr', name: 'Hubert', surname: 'Schmidt',
+    email: 'peter.meier99@gmx.de', phone: '+4915233742229', nationality: 'deutsch', address: {
+      country: 'Deutschland', postalcode: '33609', city: 'Bielefeld', street: 'Hakenort 45'
+    }, contactform: ['E-Mail', 'Telefon']
   },
   {
-    id: '04', name: 'Alexa', surname: 'Bezos', email: 'peter.meier99@gmx.de', phone: '+4915233742229', address: {
-      postalcode: '18565', city: 'Insel Hiddensee', street: 'Kirchweg 39'
-    }
+    id: '04', salutation: 'Herr/Frau', name: 'Alexa', surname: 'Bezos',
+    email: 'peter.meier99@gmx.de', phone: '+4915233742229', nationality: 'deutsch', address: {
+      country: 'Deutschland', postalcode: '18565', city: 'Insel Hiddensee', street: 'Kirchweg 39'
+    }, contactform: ['E-Mail', 'Telefon']
   },
 ]

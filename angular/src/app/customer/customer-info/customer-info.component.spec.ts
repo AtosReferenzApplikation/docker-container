78570fd0bd5da8c9aae3ba83cd630cd496e9d0c6
Lucas Golenia <lucas.golenia@atos.net>
2019-04-15 14:56:51 +0200

    standard unit tests

--- angular/src/app/customer/customer-info/customer-info.component.spec.ts
+++ angular/src/app/customer/customer-info/customer-info.component.spec.ts
@@ -1,20 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CustomerInfoComponent } from './customer-info.component';
+import { CustomerChatComponent } from './customer-chat.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-describe('CustomerInfoComponent', () => {
-  let component: CustomerInfoComponent;
-  let fixture: ComponentFixture<CustomerInfoComponent>;
+describe('CustomerChatComponent', () => {
+  let component: CustomerChatComponent;
+  let fixture: ComponentFixture<CustomerChatComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CustomerInfoComponent ]
+      declarations: [CustomerChatComponent],
+      imports: [
+        FontAwesomeModule,
+        HttpClientTestingModule,
+        NgxSpinnerModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterModule.forRoot([]),
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CustomerInfoComponent);
+    fixture = TestBed.createComponent(CustomerChatComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- angular/src/app/customer/customer.component.spec.ts
+++ angular/src/app/customer/customer.component.spec.ts
@@ -1,12 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CustomerComponent } from './customer.component';
-import { CustomerChatModule } from './customer-chat/customer-chat.module';
-import { CustomerInfoComponent } from './customer-info/customer-info.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterTestingModule } from '@angular/router/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CustomerChatComponent } from '../customer-chat/customer-chat.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterModule } from '@angular/router';
 
 describe('CustomerComponent', () => {
   let component: CustomerComponent;
@@ -14,13 +15,18 @@ describe('CustomerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CustomerComponent, CustomerInfoComponent],
+      declarations: [CustomerComponent,
+        CustomerChatComponent],
       imports: [
+        FontAwesomeModule,
+        ReactiveFormsModule,
         NgbModule,
+        NgxSpinnerModule,
         HttpClientTestingModule,
+        NgxSpinnerModule,
+        ReactiveFormsModule,
         FormsModule,
-        RouterTestingModule,
-        CustomerChatModule
+        RouterModule.forRoot([])
       ]
     })
       .compileComponents();
@@ -29,10 +35,10 @@ describe('CustomerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CustomerComponent);
     component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture.detectChanges();
   });
 
-  it('should create component', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });

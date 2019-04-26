0e74e1a394fa7c74844eefc5b17ee4550e78aa89
LucasG04 <lucas.golenia@atos.net>
2019-04-26 10:28:59 +0200

    circuit avatar des kunden wird in der fallbearbeitung angezeigt

--- angular/src/app/customer/customer.component.ts
+++ angular/src/app/customer/customer.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { CustomerService } from '../shared/services/customer.service';
 import { Customer } from '../models/customer';
+import { CircuitService } from '../shared/services/circuit.service';
 
 @Component({
   selector: 'app-customer',
@@ -13,15 +14,23 @@ import { Customer } from '../models/customer';
 export class CustomerComponent implements OnInit {
 
   customer: Customer = null;
-  downloadJsonHref: any;
+  avatarUrl = '';
 
   constructor(private activatedRoute: ActivatedRoute,
-    private customerService: CustomerService) { }
+    private customerService: CustomerService,
+    private circuitService: CircuitService) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.customer = this.customerService.getCustomerById(params.id);
+      this.getAvatarOfCustomer(this.customer);
     });
+  }
+
+  getAvatarOfCustomer(customer: any) {
+    this.circuitService.getUserById(customer.id).then(user => {
+      this.avatarUrl = user.avatar;
+    }).catch(() => this.avatarUrl = `https://ui-avatars.com/api/name=${customer.name}+${customer.surname}`);
   }
 
 }

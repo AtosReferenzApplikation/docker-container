31bbe60b0a8c4ac5b54d131653beb554b36bc5a3
LucasG04 <lucas.golenia@atos.net>
2019-04-26 10:44:28 +0200

    sample ids an circuit ids angepasst

--- angular/src/app/shared/sample-customers.ts
+++ angular/src/app/shared/sample-customers.ts
@@ -9,19 +9,19 @@ export const SAMPLE_CUSTOMERS: Customer[] = [
     }, contactform: ['E-Mail', 'Telefon', 'Post']
   },
   {
-    id: '02', salutation: 'Frau', name: 'Sabine', surname: 'Blau',
+    id: '6d74f358-f641-493f-a433-ad03786503ca', salutation: 'Frau', name: 'Sabine', surname: 'Blau',
     email: 'sabine.blau26@mailinator.com', phone: '+4915233742229', nationality: 'deutsch', address: {
       country: 'Deutschland', postalcode: '08523', city: 'Plauen', street: 'Bergstraße 36'
     }, contactform: ['E-Mail', 'Telefon', 'Post']
   },
   {
-    id: '03', salutation: 'Herr', name: 'Hubert', surname: 'Schmidt',
+    id: '72f17761-1ad0-460b-85ba-a21dace1d9cf', salutation: 'Herr', name: 'Hubert', surname: 'Schmidt',
     email: 'huber.schmidt45@mailinator.com', phone: '+4915233742229', nationality: 'deutsch', address: {
       country: 'Deutschland', postalcode: '33609', city: 'Bielefeld', street: 'Hakenort 45'
     }, contactform: ['E-Mail', 'Telefon']
   },
   {
-    id: '04', salutation: 'Herr/Frau', name: 'Alexa', surname: 'Bezos',
+    id: 'a2ca49b6-8af7-44c0-91d8-c9121aa94e48', salutation: 'Herr/Frau', name: 'Alexa', surname: 'Bezos',
     email: 'alexa.bezos02@mailinator.com', phone: '+4915233742229', nationality: 'deutsch', address: {
       country: 'Deutschland', postalcode: '18565', city: 'Insel Hiddensee', street: 'Kirchweg 39'
     }, contactform: ['E-Mail', 'Telefon']
